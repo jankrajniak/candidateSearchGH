1ff57881fc370b2cb2201749a7d88506453a9529
jankrajniak <jan.krajniak@gmail.com>
2025-01-25 18:15:31 -0500

    Candidate type created. Navigation menu created. Implementing integration of Github search and candidate details API next.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,30 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+
+  const links = [
+    { key: 1, label: "Home", to: "/"},
+    { key: 2, label: "Potential Candidates", to: "/SavedCandidates"},
+  ];
+
+  const location = useLocation();
+
   return (
-    <div>Nav</div>
+    <div className = "header">
+      <nav className = "navbar navbar-expand-sm">
+        <div className = "container-fluid">
+          <div className = "collapse navbar-collapse">
+            <ul className = "navbar-nav me-auto mb-2 mb-md-0">
+              {links.map((link) => (
+                <li className = "nav-item" key={link.key}>
+                  <Link to={link.to} className={`nav-link ${location.pathname== link.to ? "text-white fw-bold" : "text-white"}`}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly name: string | null;
+    readonly login: string,
+    readonly avatar_url: string,
+    readonly company: string | null,
+    readonly location: string | null,
+    readonly bio: string | null,
+    readonly hireable: boolean,
+}
